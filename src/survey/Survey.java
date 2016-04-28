9fb96991e46ab15788f204192e8bf7014a0de0bd
gavlasj <gaskosk@gmail.com>
2016-04-29 01:04:51 +0200

    prvy pokus

--- src/survey/Survey.java
+++ src/survey/Survey.java
@@ -16,7 +16,7 @@ public class Survey {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("survey?");
+        System.out.println("survey??");
     }
     
 }
